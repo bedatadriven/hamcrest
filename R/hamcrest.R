fb82e11a92c1b3b8c1d15d120a73ad31ac6b43b1
Maarten-Jan Kallen <mj@bedatadriven.com>
2014-05-22 15:07:08 +0200

    initial commit of full package source

---
+++ R/hamcrest.R
@@ -0,0 +1,65 @@
+
+# --------------------------------------
+# ASSERTION FUNCTION
+# --------------------------------------
+
+assertThat <- function(actual, matcher) {
+	if(!matcher(actual)) {
+		stop("\nExpected: ", deparse(substitute(matcher)), "\nGot: ", deparse(actual))
+	}
+}
+
+# --------------------------------------
+# MATCHER FUNCTIONS
+# --------------------------------------
+
+closeTo <- function(expected, delta) {
+	stopifnot(is.numeric(expected) & is.numeric(delta) & length(delta) == 1L)
+	function(actual) {
+		length(expected) == length(actual) &&
+				all(abs(expected-actual)<delta)	
+	}
+}
+
+identicalTo <- function(expected) {
+	function(actual) {
+		identical(expected, actual)
+	}
+}
+
+equalTo <- function(expected) {
+	function(actual) {
+		length(actual) == length(expected) &&
+				actual == expected
+	}
+}
+
+instanceOf <- function(expected) {
+    function(actual) {
+        inherits(actual, expected)
+    }
+}
+
+isTrue <- function() {
+    function(actual) {
+        identical(TRUE, actual)
+    }
+}
+
+isFalse <- function() {
+    function(actual) {
+        identical(FALSE, actual)
+    }
+}
+
+# --------------------------------------
+# ABBREVIATIONS
+# --------------------------------------
+
+assertTrue <- function(value) {
+    assertThat(value, isTrue())
+}
+
+assertFalse <- function(value) {
+    assertThat(value, isFalse())
+}
